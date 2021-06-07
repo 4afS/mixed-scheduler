1d458ebf2e59e3aa93dbcf5ef72a92373f516110
4afS <4afS.h1@gmail.com>
2021-06-08 07:34:30 +0900

    add validation

--- file/schedule/schedule.go
+++ file/schedule/schedule.go
@@ -1,9 +1,10 @@
 package schedule
 
 import (
-	"fmt"
 	"io/ioutil"
+	"regexp"
 
+	"github.com/go-playground/validator"
 	"gopkg.in/yaml.v2"
 )
 
@@ -13,14 +14,32 @@ func LoadScheduleFile(path string) (string, error) {
 }
 
 type Schedule struct {
-	Base string `yaml:"base"`
-	Plan []Plan `yaml:"plan"`
+	Base string `yaml:"base" validate:"required,time"`
+	Plan []Plan `yaml:"plan" validate:"dive"`
 }
 
 type Plan struct {
-	StartAt string `yaml:"start"`
-	Term    int    `yaml:"term,omitempty"`
-	Title   string `yaml:"title"`
+	StartAt string `yaml:"start" validate:"required,time"`
+	Term    int    `yaml:"term" validate:"min=0,max=1440,numeric"`
+	Title   string `yaml:"title" validate:"required"`
+}
+
+func (s *Schedule) validate() error {
+	validate := validator.New()
+	if err := validate.RegisterValidation("time", ValidateTime); err != nil {
+		return err
+	}
+
+	if err := validate.Struct(s); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ValidateTime(fl validator.FieldLevel) bool {
+	timeRegex := regexp.MustCompile("^([01]?[0-9]|2[0-3]):([0-5]?[0-9])$")
+	return timeRegex.MatchString(fl.Field().String())
 }
 
 func Parse(loaded string) (Schedule, error) {
@@ -28,11 +47,11 @@ func Parse(loaded string) (Schedule, error) {
 
 	err := yaml.UnmarshalStrict([]byte(loaded), &schedule)
 	if err != nil {
-		return Schedule{}, err
+		return schedule, err
 	}
 
-	if schedule.Base == "" {
-		return Schedule{}, fmt.Errorf("base time is empty")
+	if err = schedule.validate(); err != nil {
+		return schedule, err
 	}
 
 	return schedule, nil
